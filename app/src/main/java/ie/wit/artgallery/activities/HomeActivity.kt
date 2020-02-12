b5b01fd8fff9b514d65d27cb31d451d5c3f89a52
PersonaForHire <conoraskins104@gmail.com>
2020-02-12 03:19:43 +0000

    im done this is it for me

--- app/src/main/java/ie/wit/artgallery/activities/HomeActivity.kt
+++ app/src/main/java/ie/wit/artgallery/activities/HomeActivity.kt
@@ -1,12 +1,33 @@
 package ie.wit.artgallery.activities
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import ie.wit.artgallery.R
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.sign_out_button
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.util.*
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,6 +35,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupUI()
+
     }
 
     private fun setupUI() {
